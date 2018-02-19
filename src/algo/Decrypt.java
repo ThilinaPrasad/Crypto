302027588774950ac5c67e168182268ce2a9164c
Thilina Prasad <thilinaprasad.15@cse.mrt.ac.lk>
2018-02-20 01:12:12 +0530

    Initial Commit

---
+++ src/algo/Decrypt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Thilina Prasad
+ */
+public class Decrypt {
+
+    public ArrayList<String> decrypt(ArrayList<String> input) {
+        return input;
+    }
+}
---
+++ src/algo/Encrypt.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Thilina Prasad
+ */
+public class Encrypt {
+    public  ArrayList<String> encrypt(ArrayList<String> input){
+        return input;
+    }
+}
